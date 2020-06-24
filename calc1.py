32df45d43132b50b30286b83b91b68c620307cba
charltonmurphy <charltonmurphy@yahoo.com>
2020-06-24 16:06:30 -0700

    idk

--- calc1.py
+++ calc1.py
@@ -1,1 +1,22 @@
+import math
 
+def add(a, b, c, d):
+    return(a+b+c+d)
+
+def subtract(a, b, c, d,):
+    return(a-b-c-d)
+
+def multiply(a, b, c, d,):
+    return(a*b*c*d)
+
+def divide(a, b, c, d):
+    return(a/b/c/d)
+a = 8
+b = 4
+c = 2
+d = 1
+
+print (divide(a, b, c, d))
+print (multiply(a, b, c, d))
+print (subtract(a, b, c, d))
+print (add(a, b, c, d))
