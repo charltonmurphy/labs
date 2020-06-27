5b1d22d43296aabb1fdc8f74dc0f145bbdb1360b
charltonmurphy <charltonmurphy@yahoo.com>
2020-06-26 17:19:08 -0700

    new version

--- calc1.py
+++ calc1.py
@@ -13,51 +13,44 @@ def multiply(a, b, c, d,):
 def divide(a, b, c, d):
     return(a/b/c/d)
 
+num = (input(" Please give me a number between 1 and 4: "))
+if (num >= '5'):
+    print ('Your number is too high please restart')
+elif (num <= '0'):
+    print ('No dude, that is too low, or its not a number, please restart')
+else:
 
+    arr = []
 
-a = 8
-b = 4
-c = 2
-d = 1
-num = int(input(" Please give me a number between 1 and 4: "))
+    for i in range(4):
+        x = (input("Enter a whole  number: "))
+        arr.append(x)
+    if arr[0] == "":
+        arr[0] = 8
+    if arr[1] == "":
+        arr[1] = 4
+    if arr[2] == "":
+        arr[2] = 2
+    if arr[3] == "":
+        arr[3] = 1
 
-
-arr = []
-
-for i in range(4):
-    x = int(input("Enter a whole  number: "))
-    arr.append(x)
-if arr[0] == 0 or []:
-    arr[0] = 8
-if arr[1] == 0 or []:
-    arr[1] = 4
-if arr[2] == 0 or []:
-    arr[2] = 2
-if arr[3] == 0 or []:
-    arr[3] = 1
-
-a = arr[0]
-b = arr[1]
-c = arr[2]
-d = arr[3]
+    a = int(arr[0])
+    b = int(arr[1])
+    c = int(arr[2])
+    d = int(arr[3])
    
-print (arr)
+    print (arr)
 
 
-if num == 1:
-    print (add(a, b, c, d))
-elif num == 2:
-    print (subtract(a, b, c,d))
-elif num == 3:
-    print (multiply(a, b, c, d))
-elif num == 4:
-    print (divide(a, b, c, d))
-elif num >= 5:
-    print ('You number is too high')
-elif num == 0:
-    print ('No dude that is too low')
-elif num == []: 
-    print ("YOU ARE A DUMBASS!!!!")
+    if num == '1':
+        print (add(a, b, c, d))
+    elif num == '2':
+        print (subtract(a, b, c,d))
+    elif num == '3':
+        print (multiply(a, b, c, d))
+    elif num == '4':
+        print (divide(a, b, c, d))
+    
 
 
 
