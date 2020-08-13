c8e7a5e99e365dfdb2f1a6e0354586a8dde12cc8
chalrtonmurphy <charltonmurphy@yahoo.com>
2020-08-12 20:18:06 -0700

    touter

---
+++ parScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#output=$(grep "ID" ex.json | cut -d: -f2)
+#echo "The value of ID is: "$output
+echo -n "Please type the key of the value you want: "; read x 
+output=$(grep "$x" ex.json | cut -d: -f2)
+output2=$(grep "$x" ex.json | cut -d: -f3)
+echo "The value of "$x" is: "$output $output2
+echo -n "Please type the value of the key you want: "; read y
+output1=$(grep "$y" ex.json | cut -d: -f1)
+echo "The key of "$y" is: " $output1
+echo "OH HELL YEA!!!"
