c1e2d18e72d9c077cb043bca9e199b1bdc26bf76
charltonmurphy <charltonmurphy@yahoo.com>
2020-08-13 23:21:33 -0700

    just saving

---
+++ playing.py
@@ -0,0 +1,13 @@
+Hello_World = ('Hello World')
+print ('Hi you')
+
+
+if ('Hello World'):
+    print ("HI back at you")
+else:
+    print ("WHAT")
+print ('you are an imposter!!')
+print ('how is this printing the if statemnet and not the else...... aaahhhhh')
+
+  # so now it prints hellow world but it shouldnt. this is frustrating as hell
+  # Try to fix this if else statment... it should be easy      
