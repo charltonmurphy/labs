09341e52d4d8a0ab2685236aa95d25eed6c64045
charltonmurphy <charltonmurphy@yahoo.com>
2020-08-13 17:44:05 -0700

    if statement

--- parScript.sh
+++ parScript.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-open ex.json
-#output=$(grep "ID" ex.json | cut -d: -f2)
-#echo "The value of ID is: "$output
-echo -n "Please type the key of the value you want: "; read x 
-output=$(grep "$x" ex.json | cut -d: -f2)
-output2=$(grep "$x" ex.json | cut -d: -f3)
-echo "The value of "$x" is: "$output $output2
-echo -n "Please type the value of the key you want: "; read y
-output1=$(grep "$y" ex.json | cut -d: -f1)
-echo "The key of "$y" is: " $output1
-echo "OH HELL YEA!!!"
+
+readonly outFile=output.txt
+echo -n "Please type file name: "; read file
+
+if [ -f $file ]; then 
+	
+	echo -n "Please type in the key for the value you want to find: "; read x
+	output="$(grep "$x" "$file" | cut -d: -f2,3)"
+	echo  "The value is: "$output > $outFile
+	
+	echo -n "Please type in the value for the Key you want to find: "; read y
+	output1="$(grep "$y" "$file" | cut -d: -f1)"
+	echo  "The key is: "$output1 >> $outFile
+else 
+	echo "$file does not exist!" > $outFile
+	exit
+fi
